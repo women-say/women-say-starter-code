5c3a9b4e3d1e7a6680c5406105cc53e47eaacb0c
Alessio Marinoni <alessio.marinoni1@gmail.com>
2019-05-28 11:36:06 +0200

    Some js functionality over the index

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,29 @@
+const postWindow = document.querySelector("#post")
+const aboutWindow = document.querySelector("#about")
+const postButton = document.querySelector(".post-icon")
+const aboutButton = document.querySelector(".about-icon")
+const closePostBtn = document.querySelector("#close-btn-post .close-btn")
+const closeAboutBtn = document.querySelector("#close-btn-about .close-btn")
+
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
+
+  postButton.onclick = () => {
+    postWindow.style.display = "block";
+  };
+
+  aboutButton.onclick = () => {
+    aboutWindow.style.display = "block";
+  };
+
+  closePostBtn.onclick = () => {
+    postWindow.style.display = "none";
+  };
+
+  closeAboutBtn.onclick = () => {
+    aboutWindow.style.display = "none";
+  };
+
 
 }, false);
