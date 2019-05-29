4f45831b401142189aa515a573bd01ec2a29a7e4
Alessio Marinoni <alessio.marinoni1@gmail.com>
2019-05-29 13:08:49 +0200

    Front page done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,28 +1,50 @@
+const bgIntro = document.querySelector(".intro-bg")
 const postWindow = document.querySelector("#post")
 const aboutWindow = document.querySelector("#about")
 const postButton = document.querySelector(".post-icon")
 const aboutButton = document.querySelector(".about-icon")
-const closePostBtn = document.querySelector("#close-btn-post .close-btn")
-const closeAboutBtn = document.querySelector("#close-btn-about .close-btn")
+const closePostBtn = document.querySelector("#close-btn-post")
+const closeAboutBtn = document.querySelector("#close-btn-about")
+const modalBg = document.querySelector(".modal-bg")
 
+
+window.onload = function () {
+  setTimeout(function () { 
+    bgIntro.style.display = "none"
+   }, 5000);
+};
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
 
   postButton.onclick = () => {
-    postWindow.style.display = "block";
+    postWindow.classList.remove("out-of-the-way-right")
+    postWindow.classList.add("move-in-right")
+    modalBg.classList.add("appear")
+    // modalBg.style.display = "block"
+    if (aboutWindow.classList.contains("move-in-left")) {
+      aboutWindow.classList.add("out-of-the-way")
+      aboutWindow.classList.remove("move-in-left")
+    }
+
   };
 
   aboutButton.onclick = () => {
-    aboutWindow.style.display = "block";
+    aboutWindow.classList.remove("out-of-the-way")
+    aboutWindow.classList.add("move-in-left")
   };
 
   closePostBtn.onclick = () => {
-    postWindow.style.display = "none";
+    postWindow.classList.add("out-of-the-way-right")
+    postWindow.classList.remove("move-in-right")
+    modalBg.classList.remove("appear")
+    // modalBg.style.display = "none"
+
   };
 
   closeAboutBtn.onclick = () => {
-    aboutWindow.style.display = "none";
+    aboutWindow.classList.add("out-of-the-way")
+    aboutWindow.classList.remove("move-in-left")
   };
 
 
