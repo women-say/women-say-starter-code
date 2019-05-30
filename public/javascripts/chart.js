9fc1d6db07d374c27ddee12b5e4ddd90785561e9
Alessio Marinoni <alessio.marinoni1@gmail.com>
2019-05-30 13:14:00 +0200

    Chart into the index

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -3,7 +3,7 @@ function legendClickCallback(event) {
 
   var target = event.target || event.srcElement;
   while (target.nodeName !== 'LI') {
-      target = target.parentElement;
+    target = target.parentElement;
   }
   var parent = target.parentElement;
   var chartId = parseInt(parent.classList[0].split("-")[0], 10);
@@ -17,82 +17,82 @@ function legendClickCallback(event) {
     target.classList.remove('hidden');
   }
 }
+const paintChart = () => {
+  axios.get("http://localhost:3000/erjason")
+    .then(response => {
 
-axios.get("http://localhost:3000/erjason")
-  .then(response => {
+      console.log("Response.data", response.data)
 
-    console.log("Response.data", response.data)
-
-    const labels = [2000, 2005, 2010, 2011, 2012, 2013, 2014, 2015]
+      const labels = [2000, 2005, 2010, 2011, 2012, 2013, 2014, 2015]
 
 
-    let datasetsArr = response.data.map((obj) => {
+      let datasetsArr = response.data.map((obj) => {
 
-      let datasets = {}
+        let datasets = {}
 
-      datasets.label = obj.Country
+        datasets.label = obj.Country
 
-      
-      datasets.fill = false,
-      datasets.hidden = true,
-      
-      datasets.data = []
-      labels.forEach(label => {
-        datasets.data.push(obj[label])
+
+        datasets.fill = false,
+          datasets.hidden = true,
+
+          datasets.data = []
+        labels.forEach(label => {
+          datasets.data.push(obj[label])
+        })
+        console.log(datasets.data)
+        if (datasets.data[7] > 1) datasets.borderColor = 'rgb(28, 114, 110)'
+        if (datasets.data[7] <= 1 && datasets.data[6] >= 0.9) datasets.borderColor = 'rgb(231, 229, 229)'
+        if (datasets.data[7] <= 0.9) datasets.borderColor = 'rgb(65, 59, 59)'
+
+        return datasets
       })
-      console.log(datasets.data)
-      if (datasets.data[7]> 1) datasets.borderColor = 'rgb(28, 114, 110)' 
-      if (datasets.data[7]<= 1 && datasets.data[6]>= 0.9) datasets.borderColor = 'rgb(231, 229, 229)' 
-      if (datasets.data[7]<= 0.9) datasets.borderColor = 'rgb(65, 59, 59)' 
-      
-      return datasets
+
+      console.log(datasetsArr)
+
+      // console.log(countries)
+
+      var ctx = document.getElementById('myChart').getContext('2d');
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: datasetsArr,
+        },
+        options: {
+          legend: {
+            fullWidth: false,
+            display: false,
+            position: 'left',
+            labels: {
+
+              boxWidth: 20,
+              fontColor: 'white',
+              fontSize: 9,
+            }
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true
+              }
+            }]
+          }
+        }
+      });
+
+      console.log(myChart.generateLegend())
+      var myLegendContainer = document.getElementById("myChartLegend");
+      // generate HTML legend
+      myLegendContainer.innerHTML = myChart.generateLegend();
+      var legendItems = myLegendContainer.getElementsByTagName('li');
+      for (var i = 0; i < legendItems.length; i += 1) {
+        legendItems[i].addEventListener("click", legendClickCallback, false);
+
+
+      }
     })
 
-    console.log(datasetsArr)
-
-    // console.log(countries)
-
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labels,
-        datasets: datasetsArr, 
-      },
-      options: {
-        legend:{
-        fullWidth: false,
-        display: false,
-        position: 'left',
-        labels: {
-          
-          boxWidth: 20,
-          fontColor:'white',
-          fontSize: 9,
-        }
-      },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-
-    console.log(myChart.generateLegend())
-    var myLegendContainer = document.getElementById("myChartLegend");
-    // generate HTML legend
-    myLegendContainer.innerHTML = myChart.generateLegend();
-    var legendItems = myLegendContainer.getElementsByTagName('li');
-    for (var i = 0; i < legendItems.length; i += 1) {
-    legendItems[i].addEventListener("click", legendClickCallback, false);
-    
-    
-  }
-})
 
 
-
-
+}
